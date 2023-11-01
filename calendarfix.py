ddf918671e32f73ff8a4f7850ecb45c92ee36d79
ZebseMelle <149425013+ZebseMelle@users.noreply.github.com>
2023-11-01 22:51:14 +0100

    Update calendarfix.py

--- calendarfix.py
+++ calendarfix.py
@@ -1,8 +1,16 @@
 from ics import Calendar, Event
+import os
+import pandas as pd
+
+path = os.path.join(os,"kalender.vcs")
+vcsfile = read_vcs(path)
 
 #General function that reads a .vcs file
 def read_vcs(file):
   with open(file,"r") as f:
     vcs_data=f.read()
   return vcs_data
+
+def remove_segment(data):
+  data
 
